97fb38052fc9fe5fe71a1c79e7bc74cfb6826be8
Rahul Kumar <rahulkr9622@gmail.com>
2020-08-23 21:13:21 +0530

    Add syncing of chains function

--- app/index.js
+++ app/index.js
@@ -6,12 +6,10 @@ const P2pServer = require('./p2p-server')
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
+const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
 
 app.use(bodyParser.json());
-
-const bc = new Blockchain();
-
-const p2pServer = new P2pServer(bc);
 
 app.get('/blocks', (req, res) => {
     res.json(bc.chain);
@@ -20,6 +18,8 @@ app.get('/blocks', (req, res) => {
 app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`New Block added: ${block.toString()}`);
+
+    p2pServer.syncChains();
 
     res.redirect('/blocks');
 });
