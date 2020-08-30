5e0331fd3c75672b489195a1b1709c6571e931ee
Rahul Kumar <rahulkr9622@gmail.com>
2020-08-30 21:34:23 +0530

    Add wallet and transactions

---
+++ wallet/index.js
@@ -0,0 +1,18 @@
+const ChainUtil = require('../chain-util');
+const { INITIAL_BALANCE } = require('../config');
+
+class Wallet {
+    constructor() {
+        this.balance   = INITIAL_BALANCE;
+        this.keyPair   = ChainUtil.genKeyPair();
+        this.publicKey = this.keyPair.getPublic().encode('hex');
+    }
+
+    toString() {
+        return `Wallet -
+            Public Key: ${this.publicKey.toString()}
+            Balance   : ${this.balance}`
+    }
+}
+
+module.exports = Wallet;
