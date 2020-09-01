28e6143b8b6e281fae1c9e96cf8009f458775315
Rahul Kumar <rahulkr9622@gmail.com>
2020-09-01 21:21:14 +0530

    Add transaction functions

--- chain-util.js
+++ chain-util.js
@@ -1,9 +1,14 @@
 const EC = require('elliptic').ec;
+const { v1: uuidV1 } = require('uuid');
 const ec = new EC('secp256k1');
 
 class ChainUtil {
     static genKeyPair() {
         return ec.genKeyPair();
+    }
+
+    static id() {
+        return uuidV1();
     }
 }
 
