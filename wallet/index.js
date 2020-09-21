11a1b1fb1c5d4b2f3a0b6ac2bb297b646bdc47c9
Rahul Kumar <rahulkr9622@gmail.com>
2020-09-21 21:41:22 +0530

    Check balance

--- wallet/index.js
+++ wallet/index.js
@@ -1,6 +1,7 @@
 const ChainUtil = require('../chain-util');
 const Transaction = require('./transaction');
 const { INITIAL_BALANCE } = require('../config');
+const { transactionWithOutputs } = require('./transaction');
 
 class Wallet {
     constructor() {
@@ -19,7 +20,9 @@ class Wallet {
         return this.keyPair.sign(dataHash);
     }
 
-    createTransaction(recipient, amount, transactionPool) {
+    createTransaction(recipient, amount, blockchain, transactionPool) {
+        this.balance = this.calculateBalance(blockchain);
+
         if (amount > this.balance) {
             console.log(`Amount: ${amount} exceeds the current balance: ${this.balance}`);
             return;
@@ -35,6 +38,39 @@ class Wallet {
         }
 
         return transaction;
+    }
+
+    calculateBalance(blockchain) {
+        let balance = this.balance;
+        let transactions = [];
+        blockchain.chain.forEach(block => block.data.forEach(transaction => {
+            transactions.push(transaction);
+        }));
+
+        const walletInputTs = transactions
+        .filter(transaction => transaction.input.address === this.publicKey);
+
+        let startTime = 0
+
+        if (walletInputTs.length > 0) {
+            const recentInputTs = walletInputTs.reduce(
+                (prev, current) => prev.input.timestamp > current.input.timestamp ? prev : current
+            );
+
+            balance = recentInputTs.outputs.find(output => output.address === this.publicKey).amount;
+            startTime = recentInputTs.input.timestamp;
+        }
+
+        transactions.forEach(transaction => {
+            if (transaction.input.timestamp > startTime) {
+                transaction.outputs.find(output => {
+                    if (output.address === this.publicKey) {
+                        balance += output.amount
+                    }
+                })
+            }
+        });
+        return balance;
     }
 
     static blockchainWallet() {
